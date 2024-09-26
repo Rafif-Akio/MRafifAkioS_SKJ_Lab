d076f670f0d3298c9218041e9b01ef774ef4cc09
Rafif-Akio <muhammadrafifakiosar@mail.ugm.ac.id>
2024-09-26 17:02:41 +0700

    Create ProvidedAssemblyinC++.cpp

---
+++ Assignment1/ProvidedAssemblyinC++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    uint16_t num1 = 5;
+    uint16_t num2 = 10;
+    uint16_t result = 0;
+
+    result = num1 * num2;
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
